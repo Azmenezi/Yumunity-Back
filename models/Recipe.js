6037e639301449d99ca55ce7db84647406c39d94
Azmenezi <azmenezi@gmail.com>
2023-07-09 17:34:03 +0100

    finished the basic CRUD

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,7 +1,7 @@
 const { model, Schema } = require("mongoose");
 
 const RecipeSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, ref: "User" },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
   text: { type: String, required: true },
   ingredients: [{ type: Schema.Types.ObjectId, ref: "Ingredient" }],
